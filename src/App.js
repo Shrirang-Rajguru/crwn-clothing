765da21f69492d340274d8e8be881d02a52606c1
ShrirangRajguru <rajgurushrirangs@gmail.com>
2021-04-13 22:37:15 +0530

    Finieshed Sign-IN handleSubmit Method

--- src/App.js
+++ src/App.js
@@ -22,29 +22,20 @@ class App extends React.Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState(
-          {
-              currentUser: {
+          this.setState({
+            currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
-          },
-          ()=>{
-            console.log(this.setState);
-          }
-          );
+          });
         });
       }
-      else{
-        this.setState({
-          currentUser: userAuth
-        });
-      }
-      
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
